b47696d79352c03792ff43fcdab0274352ca44b9
Artur Pawelczyk <pchairman@gmail.com>
2022-06-07 20:06:12 +0200

    Init file for shell

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -1,9 +1,32 @@
 use relational_nosql_lib::db::{Database, QueryResults};
 use relational_nosql_lib::parse::{parse_command, parse_query};
 use rustyline::Editor;
+use clap::Parser;
+
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
+/// Interactive DB shell
+#[derive(Parser, Debug)]
+struct Args {
+    /// Init file
+    #[clap(long)]
+    init: Option<String>,
+}
 
 fn main() {
+    let args = Args::parse();
     let mut db = Database::default();
+    if let Some(path) = args.init {
+        let mut contents = String::new();
+        let mut file = File::open(Path::new(&path)).unwrap();
+        file.read_to_string(&mut contents).unwrap();
+        for line in contents.split('\n') {
+            handle_command(&mut db, line.trim());
+        }
+    }
+
     let mut editor = Editor::<()>::new();
     loop {
         match editor.readline("query> ") {
@@ -14,7 +37,8 @@ fn main() {
 }
 
 fn handle_command(db: &mut Database, cmd: &str) {
-    if cmd.starts_with("create") {
+    if cmd.is_empty() {
+    } else if cmd.starts_with("create") {
         let command = match parse_command(cmd) {
             Ok(x) => x,
             Err(error) => { println!("{}", error); return; }
