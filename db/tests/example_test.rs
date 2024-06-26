3c8a8694412b2f9a549f8798b6922bab055d879c
Artur Pawelczyk <pchairman@gmail.com>
2024-06-26 16:39:43 +0200

    Integration test for the example code

---
+++ db/tests/example_test.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+
+use rqldb::{parse, Database};
+
+#[test]
+fn test_example_code() -> Result<(), Box<dyn Error>> {
+    let mut db = Database::default();
+
+    let file = std::fs::read("../examples/example.query")?;
+    for line in String::from_utf8(file).unwrap().lines().filter(|s| !s.is_empty()).map(str::trim) {
+        if line.starts_with("create") {
+            let command = parse::parse_command(line)?;
+            db.execute_create(&command);
+        } else {
+            let query = parse::parse_query(line)?;
+            db.execute_query(&query)?;
+        }
+    }
+
+    Ok(())
+}
