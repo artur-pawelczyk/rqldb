a9037f264b1ad7a7053fbeccde9d838856ad7ed1
Artur Pawelczyk <pchairman@gmail.com>
2024-07-14 22:58:13 +0200

    Benchmark for inserting to non-indexed relation

--- db/benches/bench.rs
+++ db/benches/bench.rs
@@ -1,13 +1,21 @@
 use criterion::{criterion_group, criterion_main, Criterion, black_box};
 use rqldb::{*, dsl::TupleBuilder};
 
-fn create_database() -> Database {
+enum DbType { PrimaryIndex, NoIndex }
+
+fn create_database(t: DbType) -> Database {
     let mut db = Database::default();
-    db.define(&Definition::relation("document")
-                      .indexed_attribute("id", Type::NUMBER)
-                      .attribute("type", Type::NUMBER)
-                      .attribute("title", Type::TEXT)
-                      .attribute("content", Type::TEXT));
+    let mut def = Definition::relation("document");
+    match t {
+        DbType::PrimaryIndex => { def = def.indexed_attribute("id", Type::NUMBER); },
+        DbType::NoIndex => { def = def.attribute("id", Type::NUMBER); },
+    }
+
+    def = def.attribute("type", Type::NUMBER)
+        .attribute("title", Type::TEXT)
+        .attribute("content", Type::TEXT);
+
+    db.define(&def);
 
     db
 }
@@ -20,7 +28,26 @@ fn query_single_number(db: &Database, query: &Query, elem: &str) -> Option<i32> 
 
 fn benchmark_insert(c: &mut Criterion) {
     c.bench_function("insert", |b| b.iter_with_setup(|| {
-        let db = create_database();
+        let db = create_database(DbType::PrimaryIndex);
+        let ids: Vec<String> = (1..1000).map(|i| i.to_string()).collect();
+
+        (db, ids)
+    }, |(db, ids)| {
+        for id in ids {
+            let query = Query::tuple(TupleBuilder::new()
+                                     .inferred("id", &id)
+                                     .inferred("type", "12")
+                                     .inferred("title", "example_doc")
+                                     .inferred("content", "the content")
+            ).insert_into("document");
+            db.execute_query(&query).unwrap();
+        }
+    }));
+}
+
+fn benchmark_insert_without_index(c: &mut Criterion) {
+    c.bench_function("insert no index", |b| b.iter_with_setup(|| {
+        let db = create_database(DbType::NoIndex);
         let ids: Vec<String> = (1..1000).map(|i| i.to_string()).collect();
 
         (db, ids)
@@ -38,7 +65,7 @@ fn benchmark_insert(c: &mut Criterion) {
 }
 
 fn benchmark_filter(c: &mut Criterion) {
-    let db = create_database();
+    let db = create_database(DbType::PrimaryIndex);
     for i in 1..100000 {
         let id = i.to_string();
         let query = Query::tuple(TupleBuilder::new()
@@ -55,7 +82,7 @@ fn benchmark_filter(c: &mut Criterion) {
 }
 
 fn benchmark_index_search(c: &mut Criterion) {
-    let db = create_database();
+    let db = create_database(DbType::PrimaryIndex);
     for i in 1..100000 {
         let id = i.to_string();
         let query = Query::tuple(TupleBuilder::new()
@@ -72,7 +99,7 @@ fn benchmark_index_search(c: &mut Criterion) {
 }
 
 fn benchmark_count(c: &mut Criterion) {
-    let db = create_database();
+    let db = create_database(DbType::PrimaryIndex);
 
     for i in 0..1_000_000 {
         let id = i.to_string();
@@ -90,7 +117,7 @@ fn benchmark_count(c: &mut Criterion) {
 }
 
 fn benchmark_read_results(c: &mut Criterion) {
-    let db = create_database();
+    let db = create_database(DbType::PrimaryIndex);
 
     for i in 0..1_000_000 {
         let id = i.to_string();
@@ -113,7 +140,7 @@ fn benchmark_read_results(c: &mut Criterion) {
 }
 
 fn benchmark_parse(c: &mut Criterion) {
-    let query = "scan example | join other example.id other.id | filter example.value > 5 | insert_into foo";
+    let query = "scan example | join example.id other.id | filter example.value > 5 | insert_into foo";
 
     c.bench_function("parse query", |b| b.iter(|| parse_query(query).unwrap()));
 }
@@ -126,5 +153,15 @@ fn benchmark_query_to_string(c: &mut Criterion) {
     c.bench_function("dump insert query", |b| b.iter(|| insert_query.to_string()));
 }
 
-criterion_group!(benches, benchmark_insert, benchmark_count, benchmark_read_results, benchmark_parse, benchmark_filter, benchmark_index_search, benchmark_query_to_string);
+criterion_group!(benches,
+                 benchmark_insert,
+                 benchmark_insert_without_index,
+                 benchmark_count,
+                 benchmark_read_results,
+                 benchmark_parse,
+                 benchmark_filter,
+                 benchmark_index_search,
+                 benchmark_query_to_string,
+);
+
 criterion_main!(benches);
