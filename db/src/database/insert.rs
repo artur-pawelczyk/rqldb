8246b70fb32eec81f8c6b874c9f438cbbebdb35b
Artur Pawelczyk <pchairman@gmail.com>
2024-08-19 22:07:23 +0200

    Move tests for insert

--- db/src/database/insert.rs
+++ db/src/database/insert.rs
@@ -24,3 +24,71 @@ impl Database {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use dsl::{Definition, Query};
+
+    use crate::{plan::Plan, Type};
+
+    use super::*;
+
+    #[test]
+    pub fn insert() {
+        let mut db = Database::default();
+
+        let command = Definition::relation("document")
+            .attribute("id", Type::NUMBER)
+            .attribute("content", Type::TEXT);
+        db.define(&command);
+
+        let insert_query = Query::build_tuple()
+            .inferred("id", "1")
+            .inferred("content", "something")
+            .build().insert_into("document");
+        let insert_result = db.insert(&insert_query);
+        assert!(insert_result.is_ok());
+
+        let query = Query::scan("document").select_all();
+        let result = db.execute_query(&query).unwrap();
+        let tuples: Vec<_> = result.tuples().collect();
+        assert_eq!(tuples.len(), 1);
+        let tuple = tuples.first().expect("fail");
+        assert_eq!(<i32>::try_from(tuple.element("document.id").unwrap()), Ok(1));
+        assert_eq!(tuple.element("document.content").unwrap().to_string(), "something");
+    }
+
+    #[test]
+    pub fn failed_insert() {
+        let mut db = Database::default();
+
+        let command = Definition::relation("document")
+            .attribute("id", Type::NUMBER)
+            .attribute("content", Type::TEXT);
+        db.define(&command);
+
+        let result = db.insert(&Query::tuple(&[("id", "not-a-number"), ("id", "random-text")]).insert_into("document"));
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn update() {
+        let mut db = Database::default();
+        db.define(&Definition::relation("document").indexed_attribute("id", Type::NUMBER).attribute("content", Type::TEXT));
+        let obj = db.object("document").unwrap();
+
+        db.insert(&Query::build_tuple()
+                  .inferred("id", "1")
+                  .inferred("content", "original content")
+                  .build().insert_into("document")).unwrap();
+        db.insert(&Query::build_tuple()
+                  .inferred("id", "1")
+                  .inferred("content", "new content")
+                  .build().insert_into("document")).unwrap();
+
+        let result = db.execute_plan(Plan::scan(obj)).unwrap();
+        let mut tuples = result.tuples();
+        assert_eq!(tuples.next().unwrap().element("document.content").unwrap().to_string(), "new content");
+        assert!(tuples.next().is_none());
+    }
+}
--- db/src/database/mod.rs
+++ db/src/database/mod.rs
@@ -254,44 +254,6 @@ mod tests {
     }
 
     #[test]
-    pub fn insert() {
-        let mut db = Database::default();
-
-        let command = Definition::relation("document")
-            .attribute("id", Type::NUMBER)
-            .attribute("content", Type::TEXT);
-        db.define(&command);
-
-        let insert_query = Query::build_tuple()
-            .inferred("id", "1")
-            .inferred("content", "something")
-            .build().insert_into("document");
-        let insert_result = db.insert(&insert_query);
-        assert!(insert_result.is_ok());
-
-        let query = Query::scan("document").select_all();
-        let result = db.execute_query(&query).unwrap();
-        let tuples: Vec<_> = result.tuples().collect();
-        assert_eq!(tuples.len(), 1);
-        let tuple = tuples.first().expect("fail");
-        assert_eq!(<i32>::try_from(tuple.element("document.id").unwrap()), Ok(1));
-        assert_eq!(tuple.element("document.content").unwrap().to_string(), "something");
-    }
-
-    #[test]
-    pub fn failed_insert() {
-        let mut db = Database::default();
-
-        let command = Definition::relation("document")
-            .attribute("id", Type::NUMBER)
-            .attribute("content", Type::TEXT);
-        db.define(&command);
-
-        let result = db.insert(&Query::tuple(&[("id", "not-a-number"), ("id", "random-text")]).insert_into("document"));
-        assert!(result.is_err());
-    }
-
-    #[test]
     pub fn filter() {
         let mut db = Database::default();
         db.define(&Definition::relation("document").attribute("id", Type::NUMBER).attribute("content", Type::TEXT));
@@ -439,27 +401,6 @@ mod tests {
         let first = result.tuples().next().unwrap();
         let count = first.element("count").unwrap();
         assert_eq!(count.try_into(), Ok(1));
-    }
-
-    #[test]
-    fn update() {
-        let mut db = Database::default();
-        db.define(&Definition::relation("document").indexed_attribute("id", Type::NUMBER).attribute("content", Type::TEXT));
-        let obj = db.object("document").unwrap();
-
-        db.insert(&Query::build_tuple()
-                  .inferred("id", "1")
-                  .inferred("content", "original content")
-                  .build().insert_into("document")).unwrap();
-        db.insert(&Query::build_tuple()
-                  .inferred("id", "1")
-                  .inferred("content", "new content")
-                  .build().insert_into("document")).unwrap();
-
-        let result = db.execute_plan(Plan::scan(obj)).unwrap();
-        let mut tuples = result.tuples();
-        assert_eq!(tuples.next().unwrap().element("document.content").unwrap().to_string(), "new content");
-        assert!(tuples.next().is_none());
     }
 
     #[test]
